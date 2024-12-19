1fc37d53a98bffdd40302380e09a9282c2f05f90
OmarDiaz14 <omar2001val@gmail.com>
2024-12-19 16:54:58 -0600

    CORS

--- lga/settings.py
+++ lga/settings.py
@@ -88,10 +88,31 @@ REST_FRAMEWORK = {
 
 }
 
-CORS_ALLOW_ALL_ORIGINS = True
-CORS_ALLOWED_ORIGINS = ["http://localhost:5173"]
 
+CORS_ALLOWED_ORIGINS = ["http://localhost:5173",
+                        "http://localhost:3000",
+                        "http://127.0.0.1:5173"]
 
+CORS_ALLOW_METHODS = [
+    'DETELE',
+    'GET',
+    'OPTIONS',
+    'PATCH',
+    'POST',
+    'PUT',
+]
+
+CORS_ALLOW_HEADERS = [
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
+]
 
 ROOT_URLCONF = 'lga.urls'
 
