8ac47aef061e9b74873a031a268f9eb1d4d6ffb7
OmarDiaz14 <omar2001val@gmail.com>
2025-01-23 16:11:07 -0600

    Seccion perfil
    se puede ver el nombre de la seccion para el perfil en el front

--- lga/settings.py
+++ lga/settings.py
@@ -119,22 +119,22 @@ WSGI_APPLICATION = 'lga.wsgi.application'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'lga_test_db',
-        'USER': 'team',
-        'PASSWORD': '@pwdlga2024',
-        'HOST': '52.118.210.246',
-        'PORT': '5432',
-    }
     # 'default': {
     #     'ENGINE': 'django.db.backends.postgresql',
-    #     'NAME': 'lga_main',
+    #     'NAME': 'lga_test_db',
     #     'USER': 'team',
     #     'PASSWORD': '@pwdlga2024',
     #     'HOST': '52.118.210.246',
     #     'PORT': '5432',
     # }
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'lga_main',
+        'USER': 'team',
+        'PASSWORD': '@pwdlga2024',
+        'HOST': '52.118.210.246',
+        'PORT': '5432',
+    }
     # 'default': {
     #     'ENGINE': 'django.db.backends.postgresql',
     #     'NAME': 'alfresco',
